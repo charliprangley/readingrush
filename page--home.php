bbff69005f9f4d02c8e07c1f2afa93a7db33a95a
Charli Prangley <charliprangley@gmail.com>
2019-06-19 10:03:05 -0400

    logged in content on homepage

--- page--home.php
+++ page--home.php
@@ -8,6 +8,10 @@ get_header();
 
 $readathon_dates = get_field('readathon_dates');
 $intro_video = get_field('intro_video');
+$loggedin_heading = get_field('loggedin_heading');
+$loggedin_subheading = get_field('loggedin_subheading');
+$loggedin_link = get_field('loggedin_link');
+$loggedin_button = get_field('loggedin_button');
 
 
 
@@ -17,13 +21,22 @@ $intro_video = get_field('intro_video');
   <div class="row flex-row mt50 home-header">
     <div class="col-sm-6">
       <h3 class="dates"><?php echo $readathon_dates; ?></h3>
-      <?php
-        while ( have_posts() ) : the_post();
-        the_title( '<h1>', '</h1>' );
-        the_content();
-        endwhile; // End of the loop.
-      ?>
-      <a href="/register" class="btn btn--purple btn-xl">Sign up now</a>
+      <?php if (!is_user_logged_in()) : ?>
+        <?php
+          while ( have_posts() ) : the_post();
+          the_title( '<h1>', '</h1>' );
+          the_content();
+          endwhile; // End of the loop.
+        ?>
+        <a href="/register" class="btn btn--purple btn-xl">Sign up now</a>
+      <?php endif; ?>
+      <?php if (is_user_logged_in()) : ?>
+        <h1><?php echo $loggedin_heading; ?></h1>
+        <p class="large-body"> <?php echo $loggedin_subheading; ?></p>
+        <?php if ($loggedin_link) : ?>
+        <a href="<?php echo $loggedin_link; ?>" class="btn btn--purple btn-xl"><?php echo $loggedin_button; ?></a>
+        <?php endif; ?>
+      <?php endif; ?>
     </div>
     <div class="col-sm-6">
       <div class="video-container">
